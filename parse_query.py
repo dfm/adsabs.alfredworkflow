ff91abedff5568c359d67071cd9e58bf92f3be4a
Dan Foreman-Mackey <dan@danfm.ca>
2019-05-30 09:47:00 -0700

    Update parse_query.py

--- parse_query.py
+++ parse_query.py
@@ -3,26 +3,26 @@ import sys
 import shlex
 from collections import namedtuple
 
-def tokenize(query):
-    tokens = shlex.split(query)
-    years = []
-    authors = []
-    for token in tokens:
-        token = token.strip()
-        numbers = re.findall("[0-9]+", token)
-        years += list(int(n) for n in numbers if len(n) == 4)
-        if len(numbers) == 0:
-            authors.append(token)
-    return list(sorted(years)), authors
-
 query = sys.argv[1]
 
-years, authors = tokenize(query)
+# Tokenize the query
+tokens = shlex.split(query)
+years = []
+authors = []
+for token in tokens:
+    token = token.strip()
+    numbers = re.findall("[0-9]+", token)
+    years += list(int(n) for n in numbers if len(n) == 4)
+    if len(numbers) == 0:
+        authors.append(token)
+years = list(sorted(years))
+
+# Fail fast if there are no authors
 if len(authors) == 0:
     sys.stdout.write(query)
     sys.exit(0)
 
-# Construct the query
+# Construct the query in ADS's format
 q = []
 for author in authors:
     q.append("author:\"" + author + "\"")
@@ -33,4 +33,5 @@ elif len(years) > 1:
         min(tokens.years), max(years)))
 q = " ".join(q)
 
+# Output the query string
 sys.stdout.write(q)
