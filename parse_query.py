46c5d452862d975751717517986496cc52dbd8cd
Dan F-M <foreman.mackey@gmail.com>
2019-05-31 08:18:42 -0700

    searching in Alfred

--- parse_query.py
+++ parse_query.py
@@ -1,37 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import re
 import sys
 import shlex
-from collections import namedtuple
 
-query = sys.argv[1]
 
-# Tokenize the query
-tokens = shlex.split(query)
-years = []
-authors = []
-for token in tokens:
-    token = token.strip()
-    numbers = re.findall("[0-9]+", token)
-    years += list(int(n) for n in numbers if len(n) == 4)
-    if len(numbers) == 0:
-        authors.append(token)
-years = list(sorted(years))
+def parse_query_string(query):
+    # Tokenize the query
+    tokens = shlex.split(query)
+    years = []
+    authors = []
+    for token in tokens:
+        token = token.strip()
+        numbers = re.findall("[0-9]+", token)
+        years += list(int(n) for n in numbers if len(n) == 4)
+        if len(numbers) == 0:
+            authors.append(token)
+    years = list(sorted(years))
 
-# Fail fast if there are no authors
-if len(authors) == 0:
-    sys.stdout.write(query)
-    sys.exit(0)
+    # Fail fast if there are no authors
+    if len(authors) == 0:
+        sys.stdout.write(query)
+        sys.exit(0)
 
-# Construct the query in ADS's format
-q = []
-for author in authors:
-    q.append("author:\"" + author + "\"")
-if len(years) == 1:
-    q.append("year:{0}".format(years[0]))
-elif len(years) > 1:
-    q.append("year:[{0} TO {1}]".format(
-        min(tokens.years), max(years)))
-q = " ".join(q)
+    # Construct the query in ADS's format
+    q = []
+    for author in authors:
+        q.append("author:\"" + author + "\"")
+    if len(years) == 1:
+        q.append("year:{0}".format(years[0]))
+    elif len(years) > 1:
+        q.append("year:[{0} TO {1}]".format(
+            min(tokens.years), max(years)))
+    q = " ".join(q)
+    return q
 
-# Output the query string
-sys.stdout.write(q)
+
+if __name__ == "__main__":
+    query = " ".join(sys.argv[1:])
+    sys.stdout.write(parse_query_string(query))
---
+++ run_search.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+import calendar
+from datetime import datetime
+
+from parse_query import parse_query_string
+
+
+RATELIMIT_FILE = os.path.expanduser("~/.ads/ratelimit")
+URL_FORMAT = "https://ui.adsabs.harvard.edu/abs/{0}/abstract".format
+
+
+def return_error(text, url, sub=None):
+    error = dict(items=[dict(title=text, arg=url)])
+    if sub is not None:
+        error["items"][0]["subtitle"] = sub
+    sys.stdout.write(json.dumps(error))
+    sys.exit(0)
+
+
+def get_ratelimit():
+    if os.path.exists(RATELIMIT_FILE):
+        with open(RATELIMIT_FILE, "r") as f:
+            return json.load(f)
+    return None
+
+
+def set_ratelimit(ratelimit):
+    with open(RATELIMIT_FILE, "w") as f:
+        json.dump(ratelimit, f)
+
+
+if __name__ == "__main__":
+    try:
+        import ads
+    except ImportError:
+        return_error("Install the 'ads' Python library to enable search",
+                     "https://github.com/andycasey/ads",
+                     sub=("Or set you prefered python interpreter in the "
+                          "~/.ads/python file"))
+
+    # Fail if we're over the rate limit
+    ratelimit = get_ratelimit()
+    if ratelimit is not None:
+        current = calendar.timegm(datetime.utcnow().timetuple())
+        delta = current - int(ratelimit.get("reset", 0))
+        if ratelimit.get("remaining", "1") == "0" and delta < 0:
+            return_error("Your ADS rate limit has been reached",
+                         "https://github.com/andycasey/ads")
+
+    # Parse the query
+    query = " ".join(sys.argv[1:]).strip()
+    query_string = parse_query_string(query)
+
+    # Perform the search
+    request = ads.SearchQuery(
+        q=query_string,
+        sort="pubdate+desc",
+        fl=["title", "author", "year", "pubdate", "bibcode"],
+        max_pages=1, rows=5)
+
+    papers = []
+    for paper in request:
+        papers.append(dict(
+            title="{0} ({1})".format(paper.title[0], paper.year),
+            subtitle=", ".join(paper.author),
+            arg=URL_FORMAT(paper.bibcode),
+        ))
+
+    # Save the rate limit
+    ratelimit = request.response.get_ratelimits()
+    set_ratelimit(ratelimit)
+
+    results = dict(items=papers)
+    sys.stdout.write(json.dumps(results))
